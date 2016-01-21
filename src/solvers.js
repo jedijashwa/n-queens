24a9eed81d47c79e4ca119067f9020ae9c3b8339
Josh Riesenbach <josh.riesenbach@gmail.com>
2016-01-21 12:38:32 -0700

    Defined findNRooksSolutions using findRooksSolutions

--- src/solvers.js
+++ src/solvers.js
@@ -11,57 +11,70 @@
 // take a look at solversSpec.js to see what the tests are expecting
 
 // create all possible combinations of n queens on a board of nxn
-var makeAllBoards = function (n){
+var findRooksBoards = function (n){
   var queens = [];
   for (var i = 0; i < n; i++) {
-    queens.push({'id': i});
+    queens.push({'row': i});
   }
   var boards = [];
   
-  var getAllBoards = function(queen, row, col){
+  var moveQueens = function (queen) {
+    if (queen.row === 0) {
+      return (queen.col < queens.length);
+    } else {
+      return (queen.col !== queens[queen.row - 1].col);
+    }
+  }
+  
+  var getAllBoards = function(queen, col, currentBoard){
+    currentBoard = currentBoard || new Board({'n': queens.length});
     // if id = 0; col; 
     // store new location to queen
     // if column doesn't exist, move to next row and column 0;
-    if (col === queens.length) {
+    if (col >= queens.length) {
       queen.col = 0;
-      queen.row = row + 1;
     } else {
       // otherwise save row and column that were passed in
-      queen.row = row;
       queen.col = col;
     }
 
-    while (queen.row < n - 1 || queen.col <= queen.id){
-      if (queen.col === queens.length) {
-        queen.col = 0;
-        queen.row++;
-      } 
-      
-      if (queen.id === queens.length-1){
-        var save = new Board({'n': queens.length});
-        for (i = 0; i < queens.length; i++) {
-          save.togglePiece(queens[i].row, queens[i].col)
+    while (moveQueens(queen)){
+      currentBoard.togglePiece(queen.row, queen.col);
+      if (!currentBoard.hasAnyRooksConflicts()) {
+        if (queen.row === queens.length-1){
+          // var save = new Board({'n': queens.length});
+          // for (i = 0; i < queens.length; i++) {
+          //   save.togglePiece(queens[i].row, queens[i].col)
+          // }
+          boards.push(JSON.parse(JSON.stringify(currentBoard)));
+        } else {
+          getAllBoards(queens[queen.row + 1], queen.col + 1, currentBoard);
         }
-        boards.push(save);
-      } else {
-        getAllBoards(queens[queen.id + 1], queen.row, queen.col + 1);
       }
+      currentBoard.togglePiece(queen.row, queen.col);
       queen.col++;
+      if (queen.col >= queens.length && queen.row !== 0) {
+        queen.col = 0;
+      } 
     }     
  };
-  
+
+
   var startTime = Date.now();
-  getAllBoards(queens[0], 0, 0);
+  getAllBoards(queens[0], 0);
   console.log('elapsed time: ' + (Date.now() - startTime) * 1/1000 + ' seconds');
   return boards;
 
 };
 
-console.log(makeAllBoards(6));
+console.log(findRooksBoards(1));
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 window.findNRooksSolution = function(n) {
-  var solution = undefined; // solution will be an array of arrays that we pass to new Board
-  
+  var solution = [];
+  var solutionBoard = findRooksBoards(n)[0]; // solution will be an array of arrays that we pass to new Board
+  for (var i = 0; i < n; i++){
+    solution.push(solutionBoard[i]);
+  }
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
