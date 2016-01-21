c3ebd0bc9b523fb1fea7567b0ed9a5d807d11875
Josh Riesenbach <josh.riesenbach@gmail.com>
2016-01-20 21:29:05 -0700

    Moved getAllBoards to slover. Working for n of up to 4.

--- src/solvers.js
+++ src/solvers.js
@@ -12,11 +12,66 @@
 
 // create all possible combinations of n queens on a board of nxn
 var makeAllBoards = function (n){
+  var queens = [];
+  for (var i = 0; i < n; i++) {
+    queens.push({'id': i});
+  }
   var boards = [];
+  
+  var getAllBoards = function(queen, row, col){
+    // if id = 0; col; 
+    // store new location to queen
+    // if column doesn't exist, move to next row and column 0;
+    if (col === queens.length) {
+      queen.col = 0;
+      queen.row = row + 1;
+    } else {
+      // otherwise save row and column that were passed in
+      queen.row = row;
+      queen.col = col;
+    }      
+    
+    // if it's the final queen, save each time its passed in
+    if (queen.id === queens.length-1){
+      var save = new Board({'n': queens.length});
+      for (i = 0; i < queens.length; i++) {
+        save.togglePiece(queens[i].row, queens[i].col)
+      }
+      boards.push(save);
+    } else {
+      return getAllBoards(queens[queen.id + 1], queen.row, queen.col + 1);
+    }
+    
+    // if not base case
+    // if last queen
+    
+    //base case================
+    if (queen.row === n - 1 && queen.col === queen.id){
+      // move the closet thing not at it's base case
+      for (var i = queen.id - 1; i >= -1; i--){
+        if (i === -1){
+          return;
+        }
+        if (!(queens[i].row == n - 1 && queens[i].col == queens[i].id)) {
+          return getAllBoards(queens[i], queens[i].row, queens[i].col + 1);
+          break;
+        }
+      }
+    }
+    
+    //end base case============
+    if (queen.id == queens.length - 1){
+      // move itself 1 spot forward
+      return getAllBoards(queen, queen.row, queen.col + 1);
+    }
+  };
+  
   getAllBoards(queens[0], 0, 0);
   return boards;
-}
 
+};
+
+console.log(makeAllBoards(4));
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 window.findNRooksSolution = function(n) {
   var solution = undefined; // solution will be an array of arrays that we pass to new Board
